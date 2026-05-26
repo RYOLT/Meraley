39ee55811fd87f93e4a5b88d0e10992a4b7f344a
RYOLT <nonoyoltzin@gmail.com>
2026-05-25 21:44:26 -0600

    api debug

--- firebase.js
+++ firebase.js
@@ -34,7 +34,6 @@ import {
 
 // ─── CONFIG ──────────────────────────────────────────────────
 const firebaseConfig = {
-  apiKey: "AIzaSyC-_drZTirWMatRuo5wqx2o7aBJ6gnplPg",
   authDomain: "merme-db.firebaseapp.com",
   projectId: "merme-db",
   storageBucket: "merme-db.firebasestorage.app",
